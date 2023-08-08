d359cbbc995c45563caf03bf0e414f7d59fd659b
@jakub.bujak <jakub.bujak@globallogic.com>
2023-08-08 15:05:01 +0200

    "Fixed exercise #5 - Leap Year Calculator, wrong steps"

--- src/main/java/pl/globallogic/exersices/basic/Ex5LeapYearCalculator.java
+++ src/main/java/pl/globallogic/exersices/basic/Ex5LeapYearCalculator.java
@@ -6,13 +6,27 @@ public class Ex5LeapYearCalculator {
         System.out.println(isLeapYear(1600));
         System.out.println(isLeapYear(2017));
         System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(2020));
     }
 
-    private static boolean isLeapYear(int year) {
-        if (year < 1 || year > 9999) return false;
-        if (( year % 4 == 0 ) && ( year % 100 == 0 ) && ( year % 400 == 0 )) {
-            return true;
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
         }
-        return false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
+
 }
