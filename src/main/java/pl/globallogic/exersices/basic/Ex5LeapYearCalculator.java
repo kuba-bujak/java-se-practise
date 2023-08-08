e60ef340043b434dfc2dd814ee2a6c81e5f184b7
@jakub.bujak <jakub.bujak@globallogic.com>
2023-08-08 15:08:02 +0200

    "Fixed warning in Ex5 - Leap Year Calculator - redundant if statement"

--- src/main/java/pl/globallogic/exersices/basic/Ex5LeapYearCalculator.java
+++ src/main/java/pl/globallogic/exersices/basic/Ex5LeapYearCalculator.java
@@ -16,11 +16,7 @@ public class Ex5LeapYearCalculator {
 
         if (year % 4 == 0) {
             if (year % 100 == 0) {
-                if (year % 400 == 0) {
-                    return true;
-                } else {
-                    return false;
-                }
+                return year % 400 == 0;
             } else {
                 return true;
             }
