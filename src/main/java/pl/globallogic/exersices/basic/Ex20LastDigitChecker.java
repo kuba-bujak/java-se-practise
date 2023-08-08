de55e88cda9e28e1aa3a2da0d48ca813b9d4b6f5
@jakub.bujak <jakub.bujak@globallogic.com>
2023-08-08 17:01:47 +0200

    "Fixed warning with if statement"

--- src/main/java/pl/globallogic/exersices/basic/Ex20LastDigitChecker.java
+++ src/main/java/pl/globallogic/exersices/basic/Ex20LastDigitChecker.java
@@ -17,8 +17,7 @@ public class Ex20LastDigitChecker {
         int digit1 = number1 % 10;
         int digit2 = number2 % 10;
         int digit3 = number3 % 10;
-        if (digit1 == digit2 || digit1 == digit3 || digit2 == digit3) return true;
-        return false;
+        return digit1 == digit2 || digit1 == digit3 || digit2 == digit3;
     }
 
     private static boolean isValid(int number) {
