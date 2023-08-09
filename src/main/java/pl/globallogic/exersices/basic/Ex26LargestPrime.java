f05acb859922bec255aa6377f4405c6f7f424b7b
@jakub.bujak <jakub.bujak@globallogic.com>
2023-08-09 16:59:27 +0200

    "Added return statement to Ex26 - Largest Prime"

---
+++ src/main/java/pl/globallogic/exersices/basic/Ex26LargestPrime.java
@@ -0,0 +1,15 @@
+package pl.globallogic.exersices.basic;
+
+public class Ex26LargestPrime {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+
+    private static int getLargestPrime(int i) {
+        return i;
+    }
+}
