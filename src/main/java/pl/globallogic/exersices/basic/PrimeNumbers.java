db1f374b73a8c938bb2aa4a4b7cc7f6b57ab67cd
@jakub.bujak <jakub.bujak@globallogic.com>
2023-08-11 16:29:13 +0200

    "Solve Prime Numbers exercise"

---
+++ src/main/java/pl/globallogic/exersices/basic/PrimeNumbers.java
@@ -0,0 +1,25 @@
+package pl.globallogic.exersices.basic;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int number = 17;
+        System.out.printf("Prime numbers before '%s': \n", number);
+        isPrimeNumber(number);
+    }
+
+    private static void isPrimeNumber(int candidate) {
+
+        for (int i = 2; i < candidate; i++) {
+            if (isPrime(i)){
+                System.out.println(i + " ");
+            }
+        }
+    }
+
+    private static boolean isPrime(int candidate) {
+        for (int i = 2; i <= Math.sqrt(candidate); i++) {
+            if (candidate % i == 0) return false;
+        }
+        return true;
+    }
+}
