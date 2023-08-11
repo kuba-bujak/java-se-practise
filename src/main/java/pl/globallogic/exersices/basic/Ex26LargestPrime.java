9fc4816d7e001409f020a5c1cdeee9f44a42a0c2
@jakub.bujak <jakub.bujak@globallogic.com>
2023-08-11 16:29:37 +0200

    "Solve exercise #26 - Largest Prime"

--- src/main/java/pl/globallogic/exersices/basic/Ex26LargestPrime.java
+++ src/main/java/pl/globallogic/exersices/basic/Ex26LargestPrime.java
@@ -9,7 +9,15 @@ public class Ex26LargestPrime {
         System.out.println(getLargestPrime(-1));
     }
 
-    private static int getLargestPrime(int i) {
-        return i;
+    private static int getLargestPrime(int number) {
+        if (number <= 1) return -1;
+        int largestPrime = 2;
+        for (int i = 2; i <= number; i++) {
+            while(number % i == 0) {
+                largestPrime = i;
+                number /= i;
+            }
+        }
+        return largestPrime;
     }
 }
