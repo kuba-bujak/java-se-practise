6f774783b7f3591677f1f9ee9424dd2cb66d8bb2
Jakub Bujak <kuba.bujak0@wp.pl>
2023-12-03 15:13:12 +0100

    "Implemented Warehouse Exceptions"

--- src/main/java/pl/globallogic/university/clients/exceptions/FullWarehouseException.java
+++ src/main/java/pl/globallogic/university/clients/exceptions/FullWarehouseException.java
@@ -1,4 +1,7 @@
 package pl.globallogic.university.clients.exceptions;
 
 public class FullWarehouseException extends RuntimeException {
+    public FullWarehouseException(String message) {
+        super(message);
+    }
 }
--- src/main/java/pl/globallogic/university/clients/exceptions/ProhibitedMetalTypeException.java
+++ src/main/java/pl/globallogic/university/clients/exceptions/ProhibitedMetalTypeException.java
@@ -1,4 +1,7 @@
 package pl.globallogic.university.clients.exceptions;
 
 public class ProhibitedMetalTypeException extends RuntimeException {
+    public ProhibitedMetalTypeException(String message) {
+        super(message);
+    }
 }
